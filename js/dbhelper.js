178f7778706b7960bbbb32b5ad57315e3932c628
johannes <e1346716@student.tuwien.ac.at>
2018-07-29 12:58:01 +0200

    started to use fetch for fetching data

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,27 +8,47 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+
+  return fetch(DBHelper.DATABASE_URL)
+        .then((res) => res.json())
+        .then((data) => {
+            const restaurants = data;
+            callback(null,restaurants)
+    });
+
+//     return fetch(`${DBHelper.DATABASE_URL}`)
+// .then((res) => {
+//   return res.json();
+// }).then((res) => {
+//   const restaurants = res;
+//   callback(null,restaurants)
+// }).catch((error) => {
+//   callback(error,null)
+// })
+
+
+    //
+    // let xhr = new XMLHttpRequest();
+    // xhr.open('GET', DBHelper.DATABASE_URL);
+    // xhr.onload = () => {
+    //   if (xhr.status === 200) { // Got a success response from server!
+    //     const json = JSON.parse(xhr.responseText);
+    //     const restaurants = json.restaurants;
+    //     callback(null, restaurants);
+    //   } else { // Oops!. Got an error from server.
+    //     const error = (`Request failed. Returned status of ${xhr.status}`);
+    //     callback(error, null);
+    //   }
+    // };
+    // xhr.send();
   }
 
   /**
